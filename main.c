bbd32a383b3283679ae688fc4734ba824907b918
jacartuyvels <jacques.cartuyvels@student.uclouvain.be>
2016-05-05 23:00:24 +0200

    marche chez jacques mais segmanetation faut chez robin

--- main.c
+++ main.c
@@ -6,20 +6,19 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-struct fractal **fract_buffer;
-struct fractal *bestfract_buffer;
-int maxthreads;
-int fract_inserted;
-int fract_extracted;
+struct fractal **fract_buffer;        // Le buffer faisant le lien entre producteur et consommateur
+struct fractal *bestfract_buffer;     // Le buffer où est placée la meilleure fractale de chaque consommateur
+int maxthreads;                       // Nombre maximal de thread
+int fract_inserted;                   // Nombre de fractale insérée dans fract_buffer
+int fract_extracted;                  // Nombre de fractale extraite de fract_buffer
+int producteur_end;                   // Nombre de thread producteur ayant fini
+int consommateur_end;                 // Nombre de thread consommateur ayant fini
+
 pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
-
 pthread_mutex_t producteur_mutex;
 pthread_mutex_t consommateur_mutex;
-int producteur_end;
-int consommateur_end;
-
 
 
 //transforme un string "fract1 800 800 -0.8 0.4" en une fractale avec les
@@ -51,7 +50,7 @@ char* add_bmp(char* str)
   char *name_file = (char *)malloc(256);
   strcpy(name_file, str);
   strncat(name_file, ".bmp", 4);
-  return;
+  return name_file;
 }
 
 //print les caractéristiques d'une fractale
@@ -74,12 +73,13 @@ void insert_item(struct fractal *fract_to_insert)
 
   for(i=0; i < 2*maxthreads ; i ++)//faux !!!!!!
   {
-    printf("test enter insert\n");
     if(fract_buffer[i]==NULL)
     {
       fract_buffer[i] = fract_to_insert;
-      read_fractal(fract_buffer[i]);
       fract_inserted ++;
+      fractal_free(fract_to_insert);
+      read_fractal(fract_buffer[i]);
+      printf("Fract insérée dans buffer\n");
       break;
     }
   }
@@ -89,22 +89,18 @@ void insert_item(struct fractal *fract_to_insert)
 // et changer toutes les fonctions
 void * producteur(void* file)
 {
-  printf("test producteur\n");
    FILE *input = fopen(file, "r");
    char buf_file[256];
    while(fgets(buf_file,sizeof(buf_file),input))
    {
-     printf("test enter file\n");
      printf("buf_file : %s\n", buf_file);
      struct fractal* fract_to_send;
      fract_to_send = write_fractal(buf_file);
-     read_fractal(fract_to_send);
+     //read_fractal(fract_to_send);
      sem_wait(&empty);
-     printf("empty\n");
+     printf("Semaphore empty\n");
      pthread_mutex_lock(&mutex);
-
      insert_item(fract_to_send);
-     printf("insert\n");
      pthread_mutex_unlock(&mutex);
      sem_post(&full);
    }
@@ -118,33 +114,47 @@ void * producteur(void* file)
 
 struct fractal * extract_item()//l'argument sera un buffer
 {
+  struct fractal *fract_to_extract;
   int i;
   for(i = 0; i < 2*maxthreads; i++)//a changer
   {
-    if(!fract_buffer[i])
+    if(fract_buffer[i]!=NULL)
     {
       fract_extracted ++;
-      return fract_buffer[i];
+      fract_to_extract = fract_buffer[i];
+      fract_buffer[i] = NULL;
+      return fract_to_extract;
     }
   }
 
 }
+// renvoie 0 si fract_buffer est vide, 1 sinon
+int fract_buffer_empty(struct fractal** fract_buffer)
+{
+  for(int i = 0;i<2*maxthreads;i++)
+  {
+    if(fract_buffer[i] != NULL)
+      return 1;
+  }
+  return 0;
+}
 
-void * consommateur(void *flagsD)
+void * consommateur()
 {
   //int flag = (int)flagsD;
   struct fractal *best_fractal;
   double best_average = 0;
   int current_average;
-  while(*fract_buffer)
+  printf("yolo\n");
+  while(fract_buffer_empty(fract_buffer)==1)
   {
-
+    printf("on est rentré dans la boucle\n");
     struct fractal *fract_to_read;
 
     sem_wait(&full);
     pthread_mutex_lock(&mutex);
     fract_to_read  = extract_item();
-    printf("extract_item");
+    printf("extract_item\n");
     pthread_mutex_unlock(&mutex);
     sem_post(&empty);
     current_average = fractal_get_average(fract_to_read);
@@ -176,16 +186,15 @@ int main(int argv, char *argc[])
 {
   int err;
   fract_inserted = 0;
-  fract_extracted;
-  maxthreads = 1; //à changer par maxthreads d'argument
-  int nombre = argv;
-  int* flagsD = 0;
+  fract_extracted = 0;
+  maxthreads = 1;    //à changer par maxthreads d'argument
+  int* flagsD = 0;   // 0 si l'option -d est désactivée, 1 si activée
   int i = 0;
   int j;
   int count = 1;
   int consommateur_end = 0;
 
-/*  for(i = 0; i < nombre ; i++)
+/*  for(i = 0; i < argv ; i++)
   {
     if(strcmp(argc[i],"-d") == 0)
     {
@@ -198,79 +207,71 @@ int main(int argv, char *argc[])
       //count = count + 2;
     }
   }*/
-  fract_buffer =(struct fractal**)malloc(2*maxthreads*sizeof(struct fractal*));
+  fract_buffer = (struct fractal**)malloc(2*maxthreads*sizeof(struct fractal*));
   bestfract_buffer = (struct fractal*)malloc(sizeof(struct fractal));
-  pthread_t thread_consomateur[nombre - count];
+  pthread_t thread_consommateur[argv - count];
   pthread_t thread_producteur[maxthreads];
 
   err = sem_init(&empty, 0, 2*maxthreads); //s'arranger pour faire avec la taille du maxthreads
+  if(err != 0)
+    printf("Erreur sem_init");
   err = sem_init(&full, 0, 0);
   if(err != 0)
-  {
     printf("Erreur sem_init");
-  }
 
-  for(i =1; i<nombre; i ++)
+  // Création des threads producteurs, égal au nombre de fichiers d'entrée
+  for(i =1; i<argv; i ++)
   {
-    printf("test 1\n");
+    printf("Création producteur %x\n", i);
     err = pthread_create(&(thread_producteur[i]), NULL, &producteur, (void*)argc[i]);
     if(err != 0)
-    {
-      printf("Erreur pthread_create");
-    }
+      printf("Erreur pthread_create producteur");
   }
 
+  // Création des threads consommateurs, égal au nombre maximal de threads
   for(j = 0; j < maxthreads; j++)
   {
-    printf("test 2\n");
-    err = pthread_create(&(thread_consomateur[i]), NULL, &consommateur, (void*)flagsD);
+    printf("Création consommateur %x\n", j);
+    err = pthread_create(&(thread_consommateur[j]), NULL, &consommateur,NULL);
     if(err != 0)
-    {
-      printf("Erreur pthread_create");
-    }
-
+      printf("Erreur pthread_create consommateur");
   }
+
+
   while(1)
   {
     int end;
-    if(producteur_end == nombre - count)
+
+    // Lorsque tous les threads producteurs ont fini, on les termine
+    if(producteur_end == argv - count)
     {
-      for (i = 1; i< nombre; i++)
+      for (i = 1; i< argv; i++)
       {
         printf("test 3\n");
-        err = pthread_join(thread_consomateur[i], NULL);
+        err = pthread_join(thread_consommateur[i], NULL);
         if(err != 0)
-        {
-          printf("Erreur pthread_join");
-        }
+          printf("Erreur pthread_join consommateur");
       }
       end++;
     }
 
+    // Lorsque tous les threads consommateurs ont fini, on les termine
     if(consommateur_end == maxthreads)
     {
-      for (j = 0; j< nombre; j++)
+      for (j = 0; j< argv; j++)
       {
         printf("test 4\n");
-        err = pthread_join(thread_producteur[i], NULL);
+        err = pthread_join(thread_producteur[j], NULL);
         if(err != 0)
-        {
-          printf("Erreur pthread_join");
-        }
-
+          printf("Erreur pthread_join producteur");
       }
       end++;
-
-      while(1)
-      {
-          if(end==2);
-          {
-              printf("on est arrivé au bout\n");
-            return(EXIT_SUCCESS);
-          }
-      }
+      if(end==2);
+        {
+          printf("on est arrivé au bout\n");
+          return(EXIT_SUCCESS);
+        }
     }
-
   }
 
 }
